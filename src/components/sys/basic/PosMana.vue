e523ba37914b40474245a275c5e7437185956002
tsq <tsqdwyy@163.com>
2021-02-17 21:50:16 +0800

    职位管理-添加、删除

--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -6,19 +6,38 @@
         class="addPosInput"
         placeholder="添加职位..."
         prefix-icon="el-icon-plus"
+        @keydown.enter.native="addPosition"
         v-model="pos.name"
       >
       </el-input>
-      <el-button icon="el-icon-plus" size="small" type="primary"
+      <el-button
+        icon="el-icon-plus"
+        size="small"
+        type="primary"
+        @click="addPosition"
         >添加</el-button
       >
     </div>
     <div class="posManaMain">
       <el-table :data="positions" stripe border style="width: 70%">
+        <el-table-column type="selection" width="55"> </el-table-column>
         <el-table-column prop="id" label="编号" width="55"> </el-table-column>
         <el-table-column prop="name" label="职位名称" width="180">
         </el-table-column>
-        <el-table-column prop="createDate" width="150" label="创建时间">
+        <el-table-column prop="createDate" width="500" label="创建时间">
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+              >编辑</el-button
+            >
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              >删除</el-button
+            >
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -35,16 +54,62 @@ export default {
       positions: [],
     };
   },
+  mounted() {
+    this.initPositions();
+  },
+
+  methods: {
+    addPosition() {
+      if (this.pos.name) {
+        this.postRequest("/system/basic/pos/", this.pos).then((resp) => {
+          if (resp) {
+            this.initPositions();
+            this.pos.name = "";
+          }
+        });
+      } else {
+        this.$message.error("职位名称不能为空");
+      }
+    },
+    handleEdit(index, data) {},
+    handleDelete(index, data) {
+      this.$confirm("此操作将永久删除【"+data.name+"】, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.deleteRequest("/system/basic/pos/" + data.id).then(resp=>{
+            if(resp) {
+              this.initPositions();
+            }
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+    initPositions() {
+      this.getRequest("/system/basic/pos/").then((resp) => {
+        if (resp) {
+          this.positions = resp;
+        }
+      });
+    },
+  },
 };
 </script>
 
 <style  scoped>
-  .addPosInput {
-        width: 300px;
-        margin-right: 8px
-    }
+.addPosInput {
+  width: 300px;
+  margin-right: 8px;
+}
 
-    .posManaMain {
-        margin-top: 10px;
-    }
+.posManaMain {
+  margin-top: 10px;
+}
 </style>
