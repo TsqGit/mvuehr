be6443fd760a47d58d2a4756767dfb9ee64424f7
tsq <tsqdwyy@163.com>
2021-02-25 23:19:48 +0800

    职称管理前端页面设计

--- src/components/sys/basic/JobLevelMana.vue
+++ src/components/sys/basic/JobLevelMana.vue
@@ -1,18 +1,68 @@
 <template>
   <div>
-      3
+    <div>
+      <el-input
+        size="small"
+        v-model="jl.name"
+        style="width: 300px"
+        prefix-icon="el-icon-plus"
+        placeholder="添加职称..."
+      ></el-input>
+      <el-select
+        v-model="jl.titleLevel"
+        placeholder="职称等级"
+        size="small"
+        style="margin-left: 5px; margin-right: 5px"
+      >
+        <el-option
+          v-for="item in titleLevels"
+          :key="item"
+          :label="item"
+          :value="item"
+        >
+        </el-option>
+      </el-select>
+      <el-button size="small" type="primary" icon="el-icon-plus"
+        >添加</el-button
+      >
+    </div>
+    <div style="margin-top: 10px;">
+      <el-table :data="jls" border style="width: 80%" size = "small">
+        <el-table-column prop="id" label="编号" width="55"> </el-table-column>
+        <el-table-column prop="name" label="职称名称" width="150">
+        </el-table-column>
+        <el-table-column prop="titleLevel" label="职称级别"> </el-table-column>
+        <el-table-column prop="createDate" label="创建时间"> </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button size="small" "
+              >编辑</el-button
+            >
+            <el-button
+              size="small"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-    }
-  }
-}
+      jl: {
+        name: "",
+        titleLevel: "",
+      },
+      titleLevels: ["正高级", "副高级", "中级", "初级", "员级"],
+      jls: [],
+    };
+  },
+};
 </script>
 
 <style  scoped>
-
 </style>
--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -21,7 +21,7 @@
 export default {
   data () {
     return {
-      activeName: 'first'
+      activeName: 'joblevelmana'
     }
   },
   components:{
